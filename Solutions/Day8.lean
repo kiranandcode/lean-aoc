d767f13ada3712c9732bacb8b634136218cc2465
Kiran Gopinathan <mail@kirancodes.me>
2024-12-08 19:10:09 +0800

    part 2

--- Solutions/Day8.lean
+++ Solutions/Day8.lean
@@ -27,13 +27,16 @@ def Int.sqrt (n: Int) : Int :=
 
 def Coord.x (v: Coord) := Prod.fst v
 def Coord.y (v: Coord) := Prod.snd v
+
+def Coord.sub (v1: Coord) (v2: Coord) : Coord := (v1.x - v2.x, v1.y - v2.y)
+def Coord.add (v1: Coord) (v2: Coord) : Coord := (v1.x + v2.x, v1.y + v2.y)
+
 def Coord.antinode (v1: Coord) (v2: Coord) :=
-   let dx := v2.x - v1.x
-   let dy := v2.y - v1.y
-   (v2.x + dx, v2.y + dy)
+   let d := v2.sub v1
+   v2.add d
 
 def inBounds (g: Grid) (c: Coord) :=
-   c.x >= 0 && c.y >= 0 && c.y <= g.size && c.x <= g[0]!.size
+   c.x >= 0 && c.y >= 0 && c.y < g.size && c.x < g[0]!.size
 
 def isEmpty (g: Grid) (c: Coord) :=
    g[c.y.natAbs]![c.x.natAbs]! == '.'
@@ -67,6 +70,37 @@ def process (input: String) : Nat := Id.run $ do
 
 
 #example process testInput evaluates to 14
-#example process <$> input evaluates to 331
+#example process <$> input evaluates to 320
 
-  
+
+-- yes could compute this with maths, but loops are easier
+partial def getAllAntinodes (g: Grid) (p1: Coord) (p2: Coord) := Id.run $ do
+   let mut antiNodes := #[p1, p2]
+   let diff := p2.sub p1
+   let mut current := p2.add diff
+   while inBounds g current do
+      antiNodes := antiNodes.push current
+      current := current.add diff
+
+   let diff := p1.sub p2
+   current := p1.add diff
+   while inBounds g current do
+      antiNodes := antiNodes.push current
+      current := current.add diff
+
+   return antiNodes.toList
+
+def process' (input: String) : Nat := Id.run $ do
+  let g := input.toGrid
+  let mut antennae := extractAntennae g
+  let mut antinodes : Std.HashSet Coord := Std.HashSet.empty
+  for (_, coords) in antennae do
+     let coords := coords.toArray
+     for i in [0:coords.size] do
+        for j in [i+1:coords.size] do
+           antinodes := antinodes.insertMany (getAllAntinodes g coords[i]! coords[j]!)
+  return antinodes.size
+
+#example process' testInput evaluates to 34
+#example process' <$> input evaluates to 1157
+
